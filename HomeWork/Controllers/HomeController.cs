a5c874ab1090a380ae8aa3ce07dd2c3397ee2101
Nikita <nikita.dronoff@gmail.com>
2023-03-28 02:33:56 +0600

    - добавил вторую страницу с сылкой на просмотр списка животных

--- HomeWork/Controllers/HomeController.cs
+++ HomeWork/Controllers/HomeController.cs
@@ -22,6 +22,12 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    [HttpGet]
+    public IActionResult AnyPage()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
